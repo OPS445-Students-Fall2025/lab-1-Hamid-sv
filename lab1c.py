43841ea66ca212d1287ce0bcbb85777c7c627ede
Hamid Samadi <ssamadivaghefi@myseneca.ca>
2025-09-21 18:27:23 -0400

    Added lab1c script

---
+++ lab1c.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+"""Practice with integer objects."""
+
+
+
+# Two objects: name and age
+
+name = "Isaac"
+
+age = 72
+
+
+
+# Print out the required sentence
+
+print(name + " is " + str(age) + " years old!")

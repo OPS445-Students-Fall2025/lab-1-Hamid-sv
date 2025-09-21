c84c74a53247afa34f4fa261589cb01fc84984e7
Hamid Samadi <ssamadivaghefi@myseneca.ca>
2025-09-21 18:22:09 -0400

    Added lab1b script

---
+++ lab1b.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+"""Practice with string objects."""
+
+
+
+# Use a single object called "name" whose value is "Isaac"
+
+name = "Isaac"
+
+
+
+# Required output:
+
+print("How old are you " + name + "?")

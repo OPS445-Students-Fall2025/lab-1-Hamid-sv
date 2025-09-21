b16f9ad6b988ea8603b894f37a6e884a3152d384
Hamid Samadi <ssamadivaghefi@myseneca.ca>
2025-09-21 18:44:06 -0400

    Added lab1d script

---
+++ lab1d.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+"""Practice with math operations in Python."""
+
+
+
+# Define objects
+
+x = 10
+
+y = 2
+
+z = 5
+
+
+
+# Compute expression
+
+result = x + y * z
+
+
+
+# Print in the required format
+
+print(f"{x} + {y} * {z} = {result}")

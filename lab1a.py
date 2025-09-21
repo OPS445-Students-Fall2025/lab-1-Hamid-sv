5b4f31a1c444e4ad5db9ba431e1e081e320d6344
Hamid Samadi <ssamadivaghefi@myseneca.ca>
2025-09-21 18:20:04 -0400

    Added lab1a script

---
+++ lab1a.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+'''Description: This program will output "hello world" to the screen.'''
+
+
+
+# The first line is a shebang: tells the system to run with python3.
+
+# Lines starting with # are comments and ignored by Python.
+
+
+
+print('Hello world')
